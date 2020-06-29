fe5bfcf6bb03b467492e3d8e009fcf58bc45d3ce
simonemayers <53239742+simonemayers@users.noreply.github.com>
2020-06-29 14:20:21 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var imageArr = ["pig/pig.png", "social/social.png", "candy/candy.png", "dating/dating.png", "equifax/equifax.png", "sleep/sleep.png"]; 
+
+
+function changePicture() {
+    for(var i=0; i < imageArr.length; i++) {
+            var next = i+1; 
+            var currentImage = document.querySelectorAll(".imageSwitch").src; 
+            if(currentImage != imageArr[6]) {
+                currentImage =  imageArr[next]; 
+            } else {
+                currentImage = imageArr[next]; 
+            }
+    }
+}; 
+changePicture(); 
+    
+    
